b29fdf08a63a8efc616068d72c8293704e97fb5c
DONG <dlaehdgus23@naver.com>
2023-10-04 05:18:57 +0900

    feat : ApiAccessDeniedHandler 작성

---
+++ src/main/java/com/foodmate/backend/security/filter/handler/ApiAccessDeniedHandler.java
@@ -0,0 +1,34 @@
+package com.foodmate.backend.security.filter.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.foodmate.backend.enums.Error;
+import com.foodmate.backend.exception.AuthException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+@RequiredArgsConstructor
+@Slf4j
+public class ApiAccessDeniedHandler implements AccessDeniedHandler {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(objectMapper.writeValueAsString(new AuthException(Error.AUTHORIZATION_NOT_FOUND)));
+
+        log.error("ApiAccessDeniedHandler.handle: ", accessDeniedException);
+    }
+}
