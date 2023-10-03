44cccf5e8eb621ef246ca1167ba8bbfe7c6b353a
Chahyun <ckgus265@gmail.com>
2023-10-04 01:31:27 +0900

    feat : 사용자 email 중복체크 기능 추가(ServiceImpl 작성)

--- src/main/java/com/foodmate/backend/service/impl/MemberServiceImpl.java
+++ src/main/java/com/foodmate/backend/service/impl/MemberServiceImpl.java
@@ -1,4 +1,27 @@
 package com.foodmate.backend.service.impl;
 
-public class MemberServiceImpl {
+import com.foodmate.backend.repository.MemberRepository;
+import com.foodmate.backend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository;
+
+    /**
+     * @param email
+     * @return 현재 사용중인 email 이면 false 리턴
+     *         아니면 true
+     */
+    @Override
+    public Boolean checkDuplicateEmail(String email) {
+        if (memberRepository.findByEmail(email).isPresent()) {
+            return false;
+        }
+        return true;
+    }
 }
