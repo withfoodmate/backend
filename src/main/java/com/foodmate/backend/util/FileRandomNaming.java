bbf3b45adb2069b3626d791a822e0286236cef33
Chahyun <ckgus265@gmail.com>
2023-10-04 00:34:56 +0900

    feat : 사용자 이미지 변경 기능 추가 (FileRandomNaming 작성)

--- src/main/java/com/foodmate/backend/util/FileRandomNaming.java
+++ src/main/java/com/foodmate/backend/util/FileRandomNaming.java
@@ -1,4 +1,14 @@
 package com.foodmate.backend.util;
 
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
+
 public class FileRandomNaming {
+
+    public static String fileRandomNaming(MultipartFile file){
+        String originalFileName = file.getOriginalFilename();
+        String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+        return  UUID.randomUUID().toString() + fileExtension;
+    }
 }
