a0dcb6d9f6440ce13ca31c80e2c7346b26479207
Chahyun <ckgus265@gmail.com>
2023-10-04 00:46:10 +0900

    refactor : Member @Data 어노테이션 -> @Getter @Setter로 변경

--- src/main/java/com/foodmate/backend/entity/Member.java
+++ src/main/java/com/foodmate/backend/entity/Member.java
@@ -2,16 +2,14 @@ package com.foodmate.backend.entity;
 
 import com.foodmate.backend.enums.MemberLoginType;
 import com.foodmate.backend.enums.MemberRole;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Entity
-@Data
+@Getter
+@Setter
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
