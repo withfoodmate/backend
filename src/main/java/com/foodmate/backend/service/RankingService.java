01509ad9eb50b58ce9e56559882da74a8debaa87
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-03 20:22:19 +0900

    feat : 랭킹 API 중 좋아요 랭킹 기능 추가 (RankingService 작성)

--- src/main/java/com/foodmate/backend/service/RankingService.java
+++ src/main/java/com/foodmate/backend/service/RankingService.java
@@ -1,4 +1,12 @@
 package com.foodmate.backend.service;
 
+import com.foodmate.backend.dto.RankingDto;
+
+import java.util.List;
+
 public interface RankingService {
+
+    // 좋아요 랭킹
+    List<RankingDto.Likes> showLikesRanking();
+
 }
