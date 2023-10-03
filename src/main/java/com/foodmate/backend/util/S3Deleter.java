beee7ee2718e4803e23cd71554a0f74401badc3f
Chahyun <ckgus265@gmail.com>
2023-10-04 00:34:40 +0900

    feat : 사용자 이미지 변경 기능 추가 (S3Deleter 작성)

--- src/main/java/com/foodmate/backend/util/S3Deleter.java
+++ src/main/java/com/foodmate/backend/util/S3Deleter.java
@@ -1,4 +1,32 @@
 package com.foodmate.backend.util;
 
+import com.amazonaws.AmazonServiceException;
+import com.amazonaws.SdkClientException;
+import com.amazonaws.services.s3.AmazonS3;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
 public class S3Deleter {
+
+    private final AmazonS3 amazonS3;
+    private final String bucketName;
+
+    public S3Deleter(AmazonS3 amazonS3, @Value("${cloud.aws.s3.bucket}") String bucketName) {
+        this.amazonS3 = amazonS3;
+        this.bucketName = bucketName;
+    }
+
+    public void deleteObject(String objectKey) {
+        try {
+            // 객체 삭제를 요청합니다.
+            amazonS3.deleteObject(bucketName, objectKey);
+        } catch (AmazonServiceException e) {
+            // Amazon S3 서비스 오류 처리
+            e.printStackTrace();
+        } catch (SdkClientException e) {
+            // Amazon S3 클라이언트 오류 처리
+            e.printStackTrace();
+        }
+    }
 }
