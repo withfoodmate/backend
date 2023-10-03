4202426bf232c9554d41c0843e4ada2b68dabf1d
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-03 20:25:31 +0900

    feat : 랭킹 API 중 좋아요 랭킹 기능 추가 (MemberRepository 작성)

--- src/main/java/com/foodmate/backend/repository/MemberRepository.java
+++ src/main/java/com/foodmate/backend/repository/MemberRepository.java
@@ -4,6 +4,12 @@ import com.foodmate.backend.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
+
+    // RankingServiceImpl - 좋아요 랭킹
+    List<Member> findTop10ByOrderByLikesDesc();
+
 }
