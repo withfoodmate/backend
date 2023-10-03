c7c0b131106db2727f3c1f18f80427a3d7f70a8d
DONG <dlaehdgus23@naver.com>
2023-10-03 22:27:36 +0900

    refactor : 불필요한 코드 정리

--- src/main/java/com/foodmate/backend/entity/Member.java
+++ src/main/java/com/foodmate/backend/entity/Member.java
@@ -1,16 +1,14 @@
 package com.foodmate.backend.entity;
 
-import com.foodmate.backend.dto.MemberDto;
 import com.foodmate.backend.enums.MemberLoginType;
 import com.foodmate.backend.enums.MemberRole;
-import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.Collection;
 
 @Entity
 @Data
@@ -55,17 +53,4 @@ public class Member {
 
     private String refreshToken;
 
-    public static Member MemberDtoToMember(MemberDto.CreateMemberRequest request, String encPassword, String uuid) {
-        return Member.builder()
-                .email(request.getEmail())
-                .nickname(request.getNickname())
-                .password(encPassword)
-                .memberLoginType(MemberLoginType.GENERAL)
-                .memberRole(MemberRole.USER)
-                .registeredDate(LocalDateTime.now())
-                .updatedDate(LocalDateTime.now())
-                .emailAuthKey(uuid)
-                .isEmailAuth(false)
-                .build();
-    }
 }
