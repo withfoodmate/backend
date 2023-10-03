c20f181e96ca949dabfa82a46b168f5d6d0f23f5
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-03 20:24:11 +0900

    feat : 랭킹 API 기능 추가 시 필요한 Member 엔티티 일부 작성

--- src/main/java/com/foodmate/backend/entity/Member.java
+++ src/main/java/com/foodmate/backend/entity/Member.java
@@ -1,13 +1,25 @@
 package com.foodmate.backend.entity;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 
+@Getter
+@Entity
 public class Member {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    private String nickname;
+
+    private String image;
+
+    private long likes;
 
 }
