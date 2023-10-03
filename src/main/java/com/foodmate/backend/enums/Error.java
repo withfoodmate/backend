1be9816937de9071ba7840f3b1deb731b1248130
DONG <dlaehdgus23@naver.com>
2023-10-04 05:19:35 +0900

    feat : ApiAuthenticationEntryPoint 작성

--- src/main/java/com/foodmate/backend/enums/Error.java
+++ src/main/java/com/foodmate/backend/enums/Error.java
@@ -11,6 +11,7 @@ public enum Error {
     DELETED_USER("이미 삭제된 유저입니다.", HttpStatus.UNAUTHORIZED),
     LOGIN_FAILED("로그인에 실패하였습니다.", HttpStatus.UNAUTHORIZED),
     AUTHORIZATION_NOT_FOUND("권한이 존재하지 않습니다.", HttpStatus.UNAUTHORIZED),
+    LOGIN_REQUIRED("로그인이 필요합니다.",HttpStatus.UNAUTHORIZED),
     USER_NOT_FOUND("해당 아이디의 사용자가 없습니다.", HttpStatus.NOT_FOUND);
 
     private final String message;
---
+++ src/main/java/com/foodmate/backend/security/filter/handler/ApiAuthenticationEntryPoint.java
@@ -0,0 +1,34 @@
+package com.foodmate.backend.security.filter.handler;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.foodmate.backend.enums.Error;
+import com.foodmate.backend.exception.AuthException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+
+@RequiredArgsConstructor
+@Slf4j
+public class ApiAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    private final ObjectMapper objectMapper;
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authenticationException) throws IOException {
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write(objectMapper.writeValueAsString(new AuthException(Error.LOGIN_REQUIRED)));
+
+        log.error("ApiAuthenticationEntryPoint.handle: ", authenticationException);
+    }
+
+}
