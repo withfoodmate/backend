ad2645d4794c87f7263eb00b9e0926e4807dd509
DONG <dlaehdgus23@naver.com>
2023-10-03 22:24:07 +0900

    feat : 로그인 한 이메일 정보 확인 (KakaoOAuth2MemberService 작성)

--- src/main/java/com/foodmate/backend/repository/MemberRepository.java
+++ src/main/java/com/foodmate/backend/repository/MemberRepository.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // RankingServiceImpl - 좋아요 랭킹
     List<Member> findTop10ByOrderByLikesDesc();
+
+    // 이메일을 통해 회원 찾기
+    Optional<Member> findByEmail(String email);
 
 }
