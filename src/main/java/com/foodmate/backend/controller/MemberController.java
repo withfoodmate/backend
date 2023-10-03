f84b2b48418bf8f352b573879213278ff0546e48
Chahyun <ckgus265@gmail.com>
2023-10-04 01:31:11 +0900

    feat : 사용자 email 중복체크 기능 추가(Controller 작성)

--- src/main/java/com/foodmate/backend/controller/MemberController.java
+++ src/main/java/com/foodmate/backend/controller/MemberController.java
@@ -1,4 +1,30 @@
 package com.foodmate.backend.controller;
 
+import com.foodmate.backend.dto.MemberDto;
+import com.foodmate.backend.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+@RequiredArgsConstructor
+@Slf4j
 public class MemberController {
+
+    private final MemberService memberService;
+    /**
+     * @param request 사용자가 입력한 email
+     * @return 현재 사용중인 email이면 false
+     *         아니면 true
+     */
+    @GetMapping("/email")
+    public ResponseEntity<Boolean> checkDuplicateEmail(@RequestBody MemberDto.request request){
+        log.error(request.getEmail());
+        return ResponseEntity.ok(memberService.checkDuplicateEmail(request.getEmail()));
+    }
 }
