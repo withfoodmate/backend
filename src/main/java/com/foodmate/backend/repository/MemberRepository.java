185e8750053d6020a8c509946832228386997b3d
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-03 22:20:27 +0900

    feat : 랭킹 API 중 모임왕 랭킹 기능 추가 (MemberRepository 작성)

--- src/main/java/com/foodmate/backend/repository/MemberRepository.java
+++ src/main/java/com/foodmate/backend/repository/MemberRepository.java
@@ -1,7 +1,9 @@
 package com.foodmate.backend.repository;
 
 import com.foodmate.backend.entity.Member;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +13,15 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
     // RankingServiceImpl - 좋아요 랭킹
     List<Member> findTop10ByOrderByLikesDesc();
+
+    // RankingServiceImpl - 모임왕 랭킹
+    @Query("SELECT m.id, m.nickname, m.image, COUNT(fg.member) AS count " +
+            "FROM Member m " +
+            "JOIN FoodGroup fg ON m.id = fg.member.id " +
+            "WHERE fg.groupDateTime < CURRENT_TIMESTAMP " +
+            "AND fg.isDeleted IS NULL " +
+            "GROUP BY m.id, m.nickname, m.image " +
+            "ORDER BY COUNT(fg.member) DESC")
+    List<Object[]> findTop10MemberWithCount(Pageable pageable);
 
 }
