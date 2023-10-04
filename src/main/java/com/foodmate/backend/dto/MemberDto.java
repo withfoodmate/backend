369ae3cde93a345f22f22ea4d98422ae62ee16a8
Chahyun <ckgus265@gmail.com>
2023-10-04 17:06:31 +0900

    feat : 사용자 이메일 중복체크 기능 추가(MemberDto.Request 작성)

--- src/main/java/com/foodmate/backend/dto/MemberDto.java
+++ src/main/java/com/foodmate/backend/dto/MemberDto.java
@@ -1,4 +1,18 @@
 package com.foodmate.backend.dto;
 
+import lombok.*;
+
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
 public class MemberDto {
+
+    @Getter
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class Request{
+        private String email;
+    }
 }
