067d81e216762901c8d931157b38aff1027a105e
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-05 01:08:18 +0900

    feat : Reply 엔티티 작성

--- src/main/java/com/foodmate/backend/entity/Reply.java
+++ src/main/java/com/foodmate/backend/entity/Reply.java
@@ -1,4 +1,39 @@
 package com.foodmate.backend.entity;
 
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
 public class Reply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Comment comment;
+
+    @ManyToOne
+    private Member member;
+
+    @Lob
+    private String content;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
 }
