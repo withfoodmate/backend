9fbb7fd20476eb84c86cdb1a849002a2edf4e3b6
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-04 16:58:57 +0900

    style : ChatRoomRepository 빈칸 추가됨

--- src/main/java/com/foodmate/backend/repository/ChatRoomRepository.java
+++ src/main/java/com/foodmate/backend/repository/ChatRoomRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ChatRoomRepository extends JpaRepository<ChatRoom,Long> {
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
 }
