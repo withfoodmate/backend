0e7f7f25abf29035284718ff9b3fd9b4c8dfa625
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-04 19:36:03 +0900

    feat : 그룹 API 중 모임 상세 조회 기능에 필요한 ChatRoomRepository 메소드 작성

--- src/main/java/com/foodmate/backend/repository/ChatRoomRepository.java
+++ src/main/java/com/foodmate/backend/repository/ChatRoomRepository.java
@@ -4,6 +4,12 @@ import com.foodmate.backend.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+
+    // 모임 아이디로 채팅방 찾기
+    Optional<ChatRoom> findByFoodGroupId(Long groupId);
+
 }
