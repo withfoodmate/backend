75581955884aeace95d59318247403eb2f84d26d
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-04 16:53:49 +0900

    feat : 그룹 API 중 모임 생성 기능 추가 (GroupController 작성)

--- src/main/java/com/foodmate/backend/controller/GroupController.java
+++ src/main/java/com/foodmate/backend/controller/GroupController.java
@@ -1,4 +1,29 @@
 package com.foodmate.backend.controller;
 
+import com.foodmate.backend.dto.GroupDto;
+import com.foodmate.backend.service.GroupService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/group")
 public class GroupController {
+
+    private final GroupService groupService;
+
+    // 모임 생성
+    @PostMapping
+    public ResponseEntity<String> addGroup(Authentication authentication,
+                                           @RequestBody @Valid GroupDto.Request request) {
+        return ResponseEntity.ok(groupService.addGroup(authentication, request));
+    }
+
 }
