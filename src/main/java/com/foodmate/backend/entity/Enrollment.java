248e0cc7d9db097b5d8d568004e59997c09ea59d
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-04 19:34:16 +0900

    feat : Enrollment 엔티티 작성

--- src/main/java/com/foodmate/backend/entity/Enrollment.java
+++ src/main/java/com/foodmate/backend/entity/Enrollment.java
@@ -1,4 +1,40 @@
 package com.foodmate.backend.entity;
 
+import com.foodmate.backend.enums.EnrollmentStatus;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@EntityListeners(AuditingEntityListener.class)
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Enrollment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private FoodGroup foodGroup;
+
+    @CreatedDate
+    private LocalDateTime enrollDate;
+
+    @Enumerated(EnumType.STRING)
+    private EnrollmentStatus status;
+
+    @LastModifiedDate
+    private LocalDateTime decisionDate;
+
 }
