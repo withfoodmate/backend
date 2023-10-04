c192baef2f51c5b9f366dbf48dee0e61baeac7bb
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-04 19:31:40 +0900

    feat : 그룹 API 중 모임 상세 조회 기능 추가 (GroupDto.DetailResponse 작성)

--- src/main/java/com/foodmate/backend/dto/GroupDto.java
+++ src/main/java/com/foodmate/backend/dto/GroupDto.java
@@ -1,12 +1,16 @@
 package com.foodmate.backend.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.foodmate.backend.entity.ChatRoom;
+import com.foodmate.backend.entity.FoodGroup;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 
 public class GroupDto {
@@ -29,6 +33,55 @@ public class GroupDto {
         private String storeAddress;
         private String latitude;
         private String longitude;
+    }
+
+    @Getter
+    @Builder
+    public static class DetailResponse {
+        private Long groupId;
+        private Long memberId;
+        private String nickname;
+        private String image;
+        private String title;
+        private String name;
+        private String content;
+        private String food;
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate date;
+        @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul")
+        private LocalTime time;
+        private int maximum;
+        private int current;
+        private String storeName;
+        private String storeAddress;
+        private String latitude;
+        private String longitude;
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm", timezone = "Asia/Seoul")
+        private LocalDateTime createdDate;
+        private Long chatRoomId;
+
+        public static DetailResponse fromEntity(FoodGroup foodGroup, int current, ChatRoom chatRoom) {
+            return DetailResponse.builder()
+                    .groupId(foodGroup.getId())
+                    .memberId(foodGroup.getMember().getId())
+                    .nickname(foodGroup.getMember().getNickname())
+                    .image(foodGroup.getMember().getImage())
+                    .title(foodGroup.getTitle())
+                    .name(foodGroup.getName())
+                    .content(foodGroup.getContent())
+                    .food(foodGroup.getFood().getType())
+                    .date(foodGroup.getGroupDateTime().toLocalDate())
+                    .time(foodGroup.getGroupDateTime().toLocalTime())
+                    .maximum(foodGroup.getMaximum())
+                    .current(current)
+                    .storeName(foodGroup.getStoreName())
+                    .storeAddress(foodGroup.getStoreAddress())
+                    .latitude(Double.toString(foodGroup.getLocation().getY()))
+                    .longitude(Double.toString(foodGroup.getLocation().getX()))
+                    .createdDate(foodGroup.getCreatedDate())
+                    .chatRoomId(chatRoom.getId())
+                    .build();
+        }
     }
 
 }
