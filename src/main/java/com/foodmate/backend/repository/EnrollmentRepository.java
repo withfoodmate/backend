506468a7e0bc0afd0a1c83eba6c2ceaa5c2e3946
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-04 22:22:28 +0900

    feat : 모임 삭제 후 신청서 Status 를 모임취소로 일괄 변경하는 메소드 작성 (EnrollmentRepository)

--- src/main/java/com/foodmate/backend/repository/EnrollmentRepository.java
+++ src/main/java/com/foodmate/backend/repository/EnrollmentRepository.java
@@ -3,6 +3,8 @@ package com.foodmate.backend.repository;
 import com.foodmate.backend.entity.Enrollment;
 import com.foodmate.backend.enums.EnrollmentStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
@@ -10,5 +12,10 @@ public interface EnrollmentRepository extends JpaRepository<Enrollment, Long> {
 
     // 모임의 현재 참여인원 카운트할 때 사용
     int countByFoodGroupIdAndStatus(Long groupId, EnrollmentStatus status);
+
+    // 모임 삭제 후 Status 상태 모임취소로 일괄 변경할 때 사용
+    @Modifying
+    @Query("UPDATE Enrollment e SET e.status = :status WHERE e.foodGroup.id = :groupId")
+    void changeStatusByGroupId(Long groupId, EnrollmentStatus status);
 
 }
