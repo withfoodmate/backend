824c4c36630c5d49321f493895b9db574fc5ce55
Chahyun <ckgus265@gmail.com>
2023-10-05 01:05:46 +0900

    refactor : USER_NOT_FOUND의 message 해당 아이디의 사용자가 없습니다 -> 해당 사용자가 없습니다.

--- src/main/java/com/foodmate/backend/enums/Error.java
+++ src/main/java/com/foodmate/backend/enums/Error.java
@@ -14,7 +14,7 @@ public enum Error {
     AUTHORIZATION_NOT_FOUND("권한이 존재하지 않습니다.", HttpStatus.UNAUTHORIZED),
     LOGIN_REQUIRED("로그인이 필요합니다.", HttpStatus.UNAUTHORIZED),
     TOKEN_INVALID("유효하지 않은 토큰 입니다.", HttpStatus.UNAUTHORIZED),
-    USER_NOT_FOUND("해당 아이디의 사용자가 없습니다.", HttpStatus.NOT_FOUND),
+    USER_NOT_FOUND("해당 사용자가 없습니다.", HttpStatus.NOT_FOUND),
 
     // FoodException
     FOOD_NOT_FOUND("입력한 음식은 DB에 존재하지 않습니다.", HttpStatus.BAD_REQUEST),
