bd41b0b35756b5bf0e055b84a6c1f3c012b25d30
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-04 16:54:51 +0900

    feat : 그룹 API 중 모임 생성 기능 추가 (GroupService 작성)

--- src/main/java/com/foodmate/backend/service/GroupService.java
+++ src/main/java/com/foodmate/backend/service/GroupService.java
@@ -1,4 +1,10 @@
 package com.foodmate.backend.service;
 
+import com.foodmate.backend.dto.GroupDto;
+import org.springframework.security.core.Authentication;
+
 public interface GroupService {
+
+    String addGroup(Authentication authentication, GroupDto.Request request);
+
 }
