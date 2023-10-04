8996f48aaf633a8907f53ffe874c8de89680395b
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-04 16:54:12 +0900

    feat : 그룹 API 중 모임 생성 기능 추가 (GroupDto 작성)

--- src/main/java/com/foodmate/backend/dto/GroupDto.java
+++ src/main/java/com/foodmate/backend/dto/GroupDto.java
@@ -1,4 +1,34 @@
 package com.foodmate.backend.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
 public class GroupDto {
+
+    @Getter
+    @NotNull
+    @AllArgsConstructor
+    public static class Request {
+        private String title;
+        private String name;
+        private String content;
+        private String food;
+        @JsonFormat(pattern = "yyyy-MM-dd")
+        private LocalDate date;
+        @JsonFormat(pattern = "HH:mm", timezone = "Asia/Seoul")
+        private LocalTime time;
+        @Max(value = 8, message = "최대 인원은 8명까지 가능합니다.")
+        private int maximum;
+        private String storeName;
+        private String storeAddress;
+        private String latitude;
+        private String longitude;
+    }
+
 }
