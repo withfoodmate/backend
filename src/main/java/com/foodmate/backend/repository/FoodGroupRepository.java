4d669be3e28324bee2503343ed552d30dbe67512
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-05 22:32:53 +0900

    feat : 그룹 API 중 검색 기능 추가 (FoodGroupRepository 작성)

--- src/main/java/com/foodmate/backend/repository/FoodGroupRepository.java
+++ src/main/java/com/foodmate/backend/repository/FoodGroupRepository.java
@@ -1,11 +1,14 @@
 package com.foodmate.backend.repository;
 
+import com.foodmate.backend.dto.SearchedGroupDto;
 import com.foodmate.backend.entity.FoodGroup;
+import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Repository
@@ -19,5 +22,21 @@ public interface FoodGroupRepository extends JpaRepository<FoodGroup, Long> {
             "GROUP BY fg.storeName, fg.storeAddress " +
             "ORDER BY COUNT(*) DESC")
     List<Object[]> findTop10StoreWithCount(Pageable pageable);
+
+    // GroupServiceImpl - 검색 기능
+    @Query("SELECT new com.foodmate.backend.dto.SearchedGroupDto(" +
+            "fg.id, fg.title, fg.name, fg.groupDateTime, fg.maximum, fg.storeName, fg.storeAddress, fg.createdDate, " +
+            "m.id, m.nickname, m.image, f.type, COUNT(e.id)) " +
+            "FROM FoodGroup fg " +
+            "JOIN Member m ON fg.member.id = m.id " +
+            "JOIN Food f ON fg.food.id = f.id " +
+            "LEFT JOIN Enrollment e ON fg.id = e.foodGroup.id AND e.status = 'ACCEPT' " +
+            "WHERE (fg.title LIKE %:keyword% OR m.nickname LIKE %:keyword%) " +
+            "AND fg.groupDateTime BETWEEN :start AND :end " +
+            "AND fg.isDeleted IS NULL " +
+            "GROUP BY fg.id, fg.title, fg.name, fg.groupDateTime, fg.maximum, fg.storeName, fg.storeAddress, fg.createdDate, " +
+            "m.id, m.nickname, m.image, f.type " +
+            "ORDER BY fg.createdDate DESC")
+    Page<SearchedGroupDto> searchByKeyword(String keyword, LocalDateTime start, LocalDateTime end, Pageable pageable);
 
 }
