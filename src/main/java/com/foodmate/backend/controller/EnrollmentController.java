bfe2695eabd33ab8606aea1e03c3eca6937571f9
Chahyun <ckgus265@gmail.com>
2023-10-06 00:33:53 +0900

    feature : 내 신청 목록 조회 (EnrollmentController 작성)

--- src/main/java/com/foodmate/backend/controller/EnrollmentController.java
+++ src/main/java/com/foodmate/backend/controller/EnrollmentController.java
@@ -1,4 +1,31 @@
 package com.foodmate.backend.controller;
 
+import com.foodmate.backend.dto.EnrollmentDto;
+import com.foodmate.backend.service.EnrollmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/enrollment")
 public class EnrollmentController {
+
+    private final EnrollmentService enrollmentService;
+    /**
+     * @param status // 신청상태
+     * @param authentication // 사용자 정보
+     * @param pageable
+     * @return 신청상태에 맞는 신청정보를 가져옴
+     */
+    @GetMapping("")
+    public ResponseEntity<Page<EnrollmentDto>> getMyEnrollment(@RequestParam String status, Authentication authentication, Pageable pageable){
+        return ResponseEntity.ok(enrollmentService.getMyEnrollment(status, authentication, pageable));
+    }
 }
