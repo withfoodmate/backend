f2d386a925a4369526e9fdd153d644b35cb396cc
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-05 13:52:47 +0900

    feat : 대댓글 일괄 삭제 메소드 작성 (ReplyRepository)

--- src/main/java/com/foodmate/backend/repository/ReplyRepository.java
+++ src/main/java/com/foodmate/backend/repository/ReplyRepository.java
@@ -1,9 +1,14 @@
 package com.foodmate.backend.repository;
 
+import com.foodmate.backend.entity.Comment;
 import com.foodmate.backend.entity.Reply;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReplyRepository extends JpaRepository<Reply,Long> {
+public interface ReplyRepository extends JpaRepository<Reply, Long> {
+
+    // 해당 댓글의 대댓글 일괄 삭제
+    void deleteAllByComment(Comment comment);
+
 }
