74476474b76b2e55c8dc632de2fe736152654882
Chahyun <ckgus265@gmail.com>
2023-10-06 00:33:10 +0900

    feature : EnrollmentStatus fromString메서드 작성

--- src/main/java/com/foodmate/backend/enums/EnrollmentStatus.java
+++ src/main/java/com/foodmate/backend/enums/EnrollmentStatus.java
@@ -6,5 +6,15 @@ public enum EnrollmentStatus {
     ACCEPT,          // 수락
     REFUSE,          // 거절
     GROUP_CANCEL,    // 모임취소
-    GROUP_COMPLETE   // 모임완료
+    GROUP_COMPLETE;   // 모임완료
+
+
+    public static EnrollmentStatus fromString(String text) {
+        for (EnrollmentStatus status : EnrollmentStatus.values()) {
+            if (status.toString().equalsIgnoreCase(text)) {
+                return status;
+            }
+        }
+        return null; // 일치하는 열거형 값이 없을 경우 null 반환
+    }
 }
