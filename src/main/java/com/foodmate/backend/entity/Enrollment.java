b22fa2818e7e974a687bf7fd954e34a6045dbac2
Chahyun <ckgus265@gmail.com>
2023-10-06 16:34:50 +0900

    feature : 요청 취소 기능 (Enrollment 작성)

--- src/main/java/com/foodmate/backend/entity/Enrollment.java
+++ src/main/java/com/foodmate/backend/entity/Enrollment.java
@@ -1,10 +1,7 @@
 package com.foodmate.backend.entity;
 
 import com.foodmate.backend.enums.EnrollmentStatus;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -17,6 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Builder
 @Getter
+@Setter
 @Entity
 public class Enrollment {
 
