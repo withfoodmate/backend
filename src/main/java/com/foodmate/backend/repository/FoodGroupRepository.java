3c0513e5496f41e8a253741cb0e0bd9b546b918c
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-06 13:43:51 +0900

    feat : 그룹 API 중 내 근처 모임(거리순) 조회 기능 추가 (FoodGroupRepository 작성)

--- src/main/java/com/foodmate/backend/repository/FoodGroupRepository.java
+++ src/main/java/com/foodmate/backend/repository/FoodGroupRepository.java
@@ -2,6 +2,7 @@ package com.foodmate.backend.repository;
 
 import com.foodmate.backend.dto.SearchedGroupDto;
 import com.foodmate.backend.entity.FoodGroup;
+import org.locationtech.jts.geom.Point;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -68,5 +69,22 @@ public interface FoodGroupRepository extends JpaRepository<FoodGroup, Long> {
             "m.id, m.nickname, m.image, f.type " +
             "ORDER BY fg.createdDate DESC")
     Page<SearchedGroupDto> getAllGroupList(LocalDateTime start, LocalDateTime end, Pageable pageable);
+
+    // TODO 5km 이내 필터링 조건 다시 알아보고 수정할 것
+    // GroupServiceImpl - 거리순 조회 & 내 근처 모임
+    @Query("SELECT new com.foodmate.backend.dto.SearchedGroupDto(" +
+            "fg.id, fg.title, fg.name, fg.groupDateTime, fg.maximum, fg.storeName, fg.storeAddress, fg.createdDate, " +
+            "m.id, m.nickname, m.image, f.type, COUNT(e.id)) " +
+            "FROM FoodGroup fg " +
+            "JOIN Member m ON fg.member.id = m.id " +
+            "JOIN Food f ON fg.food.id = f.id " +
+            "LEFT JOIN Enrollment e ON fg.id = e.foodGroup.id AND e.status = 'ACCEPT' " +
+            "WHERE fg.groupDateTime BETWEEN :start AND :end " +
+            "AND fg.isDeleted IS NULL " +
+            "AND FUNCTION('ST_DISTANCE', fg.location, :userLocation) < 5000 " +
+            "GROUP BY fg.id, fg.title, fg.name, fg.groupDateTime, fg.maximum, fg.storeName, fg.storeAddress, fg.createdDate, " +
+            "m.id, m.nickname, m.image, f.type " +
+            "ORDER BY FUNCTION('ST_DISTANCE', fg.location, :userLocation)")
+    Page<SearchedGroupDto> searchByLocation(Point userLocation, LocalDateTime start, LocalDateTime end, Pageable pageable);
 
 }
