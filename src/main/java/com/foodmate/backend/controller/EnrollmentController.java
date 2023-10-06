0629daf38537c545f87f5297301dab69e93f5170
Chahyun <ckgus265@gmail.com>
2023-10-06 16:39:45 +0900

    feature : 요청 취소 기능 (EnrollmentController 작성)

--- src/main/java/com/foodmate/backend/controller/EnrollmentController.java
+++ src/main/java/com/foodmate/backend/controller/EnrollmentController.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
