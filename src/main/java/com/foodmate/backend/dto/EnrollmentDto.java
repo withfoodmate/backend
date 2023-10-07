1ce271ef16ccd9a540b89396f4a9e0bc722277e4
Chahyun <ckgus265@gmail.com>
2023-10-07 20:46:05 +0900

    refactor : 내 신청목록 조회 코드 리팩토링

--- src/main/java/com/foodmate/backend/dto/EnrollmentDto.java
+++ src/main/java/com/foodmate/backend/dto/EnrollmentDto.java
@@ -1,6 +1,7 @@
 package com.foodmate.backend.dto;
 
 import com.foodmate.backend.enums.EnrollmentStatus;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,34 +17,28 @@ import java.time.LocalTime;
 @Builder
 public class EnrollmentDto {
 
-    private Long enrollmentId;
-    private Long foodGroupId;
-    private String image;
-    private String title;
-    private String name;
-    private String type;
-    private LocalDateTime groupDateTime;
-    private int maximum;
-    private String storeName;
-    private String storeAddress;
+    @Getter
+    @AllArgsConstructor
+    @Builder
+    public static class myEnrollmentResponse{
+        private Long id;
+        private Long foodGroupId;
+        private String foodGroupMemberImage;
+        private String foodGroupTitle;
+        private String foodGroupName;
+        private String foodGroupFoodType;
+        private LocalDateTime foodGroupGroupDateTime;
+        private int foodGroupMaximum;
+        private String foodGroupStoreName;
+        private String foodGroupStoreAddress;
 
-    @Enumerated(EnumType.STRING)
-    private EnrollmentStatus status;
+        @Enumerated(EnumType.STRING)
+        private EnrollmentStatus status;
+
+    }
+
 
     // 생성자 추가
-    public EnrollmentDto(Long enrollmentId, Long foodGroupId, String image, String title, String name, String type, LocalDateTime groupDateTime,int maximum, String storeName, String storeAddress, EnrollmentStatus status) {
-        this.enrollmentId = enrollmentId;
-        this.foodGroupId = foodGroupId;
-        this.image = image;
-        this.title = title;
-        this.name = name;
-        this.type = type;
-        this.groupDateTime = groupDateTime;
-        this.maximum = maximum;
-        this.storeName = storeName;
-        this.storeAddress = storeAddress;
-        this.status = status;
-    }
 
     @Getter
     @Builder
