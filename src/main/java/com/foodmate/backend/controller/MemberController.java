bca9c150b5c42a7dee1b3c9c3fb4fcca1200c95d
Chahyun <ckgus265@gmail.com>
2023-10-07 20:47:21 +0900

    refactor : s3 위치 변경으로 인한 import 변경

--- src/main/java/com/foodmate/backend/controller/MemberController.java
+++ src/main/java/com/foodmate/backend/controller/MemberController.java
@@ -13,7 +13,6 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.List;
 import java.util.Map;
 
 @RestController
