92dfd19bce96112f04360b58bb6c94f7004648ac
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-07 16:13:01 +0900

    refactor : 랭킹 API 쿼리 수정, CURRENT_TIMESTAMP 사용 대신 LocalDateTime comparisonDate 파라미터로 LocalDateTime.now() 입력받기

--- src/main/java/com/foodmate/backend/repository/FoodRepository.java
+++ src/main/java/com/foodmate/backend/repository/FoodRepository.java
@@ -6,21 +6,22 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface FoodRepository extends JpaRepository<Food, Long> {
 
-    // RankingServiceImpl - 음식 카테고리 랭킹
+    // RankingService - 음식 카테고리 랭킹
     @Query("SELECT f.type, f.image, COUNT(fg.food) AS count " +
             "FROM Food f " +
             "JOIN FoodGroup fg ON f.id = fg.food.id " +
-            "WHERE fg.groupDateTime < CURRENT_TIMESTAMP " +
+            "WHERE fg.groupDateTime < :comparisonDate " +
             "AND fg.isDeleted IS NULL " +
             "GROUP BY f.type, f.image " +
             "ORDER BY COUNT(fg.food) DESC")
-    List<Object[]> findTop10FoodWithCount(Pageable pageable);
+    List<Object[]> findTop10FoodWithCount(LocalDateTime comparisonDate, Pageable pageable);
 
     // 음식 이름으로 푸드 엔티티 찾기
     Optional<Food> findByType(String foodName);
