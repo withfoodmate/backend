59994cbf321f5be7f01537d0ce7e9fec27fda293
DONG <dlaehdgus23@naver.com>
2023-10-09 01:03:58 +0900

    feat : 배포 테스트 (HealthController 작성)

---
+++ src/main/java/com/foodmate/backend/test/HealthController.java
@@ -0,0 +1,13 @@
+package com.foodmate.backend.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "OK!";
+    }
+}
