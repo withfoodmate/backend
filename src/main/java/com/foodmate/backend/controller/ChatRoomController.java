8c0f5963a0b20c33b05780f237c16319d3a39991
Chahyun <ckgus265@gmail.com>
2023-10-13 08:52:12 +0900

    feat : 채팅정보 (ChatRoomController 작성)

--- src/main/java/com/foodmate/backend/controller/ChatRoomController.java
+++ src/main/java/com/foodmate/backend/controller/ChatRoomController.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +25,11 @@ public class ChatRoomController {
     @GetMapping()
     public ResponseEntity<List<ChatDto.ChatRoomListResponse>> getChatRoomList(Authentication authentication) {
         return ResponseEntity.ok(chatRoomService.getChatRoomList(authentication));
+    }
+
+    @GetMapping("/{charRoomId}")
+    public ResponseEntity<ChatDto.ChatRoomInfoResponse> getChatRoomInfo(@PathVariable Long charRoomId){
+        return ResponseEntity.ok(chatRoomService.getChatRoomInfo(charRoomId));
     }
 
 }
