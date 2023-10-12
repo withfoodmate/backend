023b2a8fc92c362be7987713e29c6670492a2a63
Chahyun <ckgus265@gmail.com>
2023-10-13 07:49:13 +0900

    feat : ChatMember 작성

--- src/main/java/com/foodmate/backend/entity/ChatMember.java
+++ src/main/java/com/foodmate/backend/entity/ChatMember.java
@@ -1,4 +1,30 @@
 package com.foodmate.backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class ChatMember {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private Member member;
+
+    @ManyToOne
+    private ChatRoom chatRoom;
+
+    private LocalDateTime insertTime;
+
 }
