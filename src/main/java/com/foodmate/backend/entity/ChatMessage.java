5ce67d7df2ca2501839d16dd5ce1b70dd71f544e
Chahyun <ckgus265@gmail.com>
2023-10-13 07:50:13 +0900

    feat : 채팅리스트 (ChatMessage 작성)

--- src/main/java/com/foodmate/backend/entity/ChatMessage.java
+++ src/main/java/com/foodmate/backend/entity/ChatMessage.java
@@ -1,4 +1,31 @@
 package com.foodmate.backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
 public class ChatMessage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private ChatRoom chatRoom;
+
+    @ManyToOne
+    private Member member;
+
+    private String content;
+
+    private LocalDateTime createDateTime;
+
+
 }
