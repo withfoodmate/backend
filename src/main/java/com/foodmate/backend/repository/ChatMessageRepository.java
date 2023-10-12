1fb5cf3cd4af4e77df3a1a05d4f3bc2101f6f84c
Chahyun <ckgus265@gmail.com>
2023-10-13 07:50:24 +0900

    feat : 채팅리스트 (ChatMessageRepository 작성)

--- src/main/java/com/foodmate/backend/repository/ChatMessageRepository.java
+++ src/main/java/com/foodmate/backend/repository/ChatMessageRepository.java
@@ -1,9 +1,16 @@
 package com.foodmate.backend.repository;
 
+import com.foodmate.backend.entity.ChatMember;
 import com.foodmate.backend.entity.ChatMessage;
+import com.foodmate.backend.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.Optional;
+
 @Repository
 public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long> {
+
+    Optional<ChatMessage> findTopByChatRoomAndCreateDateTimeAfterOrderByCreateDateTimeDesc(ChatRoom chatRoom, LocalDateTime insertTime);
 }
