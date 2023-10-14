ac29a8273fd287f710ac9aa1bf99815f6683e26b
jin <2valen@naver.com>
2023-10-14 22:22:00 +0900

    test : 그룹 API 중 모임 생성 테스트 코드 추가

--- src/main/java/com/foodmate/backend/entity/Food.java
+++ src/main/java/com/foodmate/backend/entity/Food.java
@@ -1,14 +1,20 @@
 package com.foodmate.backend.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Food {
 
     @Id
