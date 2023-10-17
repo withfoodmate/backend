c92602ff85fc7519585e861edb6e50172ba8ee5f
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-17 23:29:52 +0900

    feat : 채팅 기능에 필요한 WebSocketException 클래스 작성

---
+++ src/main/java/com/foodmate/backend/exception/WebSocketException.java
@@ -0,0 +1,20 @@
+package com.foodmate.backend.exception;
+
+import com.foodmate.backend.enums.Error;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class WebSocketException extends RuntimeException {
+
+    private final Error error;
+    private final String message;
+    private final HttpStatus httpStatus;
+
+    public WebSocketException(Error error) {
+        this.error = error;
+        this.message = error.getMessage();
+        this.httpStatus = error.getHttpStatus();
+    }
+
+}
