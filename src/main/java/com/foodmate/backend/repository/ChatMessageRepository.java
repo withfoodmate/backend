e75442fae959056cc63ddce71718d1865620b018
Lee Ju-Yeon <lovedoremi90@naver.com>
2023-10-24 16:45:22 +0900

    feat : 채팅방 삭제에 필요한 메소드 작성 (ChatMessageRepository)

--- src/main/java/com/foodmate/backend/repository/ChatMessageRepository.java
+++ src/main/java/com/foodmate/backend/repository/ChatMessageRepository.java
@@ -1,6 +1,5 @@
 package com.foodmate.backend.repository;
 
-import com.foodmate.backend.entity.ChatMember;
 import com.foodmate.backend.entity.ChatMessage;
 import com.foodmate.backend.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -11,11 +10,15 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface ChatMessageRepository extends JpaRepository<ChatMessage,Long> {
+public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
 
     Optional<ChatMessage> findTopByChatRoomAndCreateDateTimeAfterOrderByCreateDateTimeDesc(ChatRoom chatRoom, LocalDateTime insertTime);
 
     int countByCreateDateTimeAfterAndChatRoom(LocalDateTime lastReadTime, ChatRoom chatRoom);
 
     List<ChatMessage> findByChatRoom_Id(Long chatRoomId);
+
+    // 해당 채팅방의 모든 데이터 삭제
+    void deleteAllByChatRoom(ChatRoom chatRoom);
+
 }
